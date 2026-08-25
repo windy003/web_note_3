5bea643bbe4a2b0133c4061827507be69738ebf0
windy003 <928920471@qq.com>
2026-08-26 05:56:18 +0800

    工具栏保持在可视区域以内

--- next.js/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverExternalPackages: ['better-sqlite3'],
-  webSocketPort: 3000,
-  allowedDevOrigins: ['127.0.0.1', 'localhost'],
-  devIndicators: false,
-};
-
-module.exports = nextConfig;
